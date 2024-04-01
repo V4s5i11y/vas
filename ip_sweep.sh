4d4e226602db9feed7145bb4bcde7bc72cc3c756
V4s5i11y <155848609+V4s5i11y@users.noreply.github.com>
2024-04-01 19:45:46 +0300

    Create ip_sweep.sh
    Simple ip sweeper from TCM Academy, thanks Heath!

---
+++ ip_sweep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#simple IP sweeper by Cyber Mentor, thanks Heath!
+if [ "$1" == "" ]
+then 
+echo "you forgot an IP address"
+echo "Syntax: ./ipsweep.sh 192.168.X"
+
+else
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+done
+fi
