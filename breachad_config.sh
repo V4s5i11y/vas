99f7db82611c3a0a8ac9a7b7c150116a4f70206f
V4s5i11y <155848609+V4s5i11y@users.noreply.github.com>
2024-06-28 16:33:23 -0400

    Update breachad_config.sh

--- breachad_config.sh
+++ breachad_config.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# thmdc-vpn-setup.sh
 
 # Check if the script is running as root
 check_root() {
@@ -21,9 +20,9 @@ check_params() {
 perform_nslookup() {
     local domain="$1"
     if nslookup "$domain" &>/dev/null; then
-        echo "nslookup $domain successful"
+        echo "[+] nslookup $domain successful"
     else
-        echo "nslookup $domain failed"
+        echo "[-] nslookup $domain failed"
     fi
 }
 
@@ -31,11 +30,15 @@ perform_nslookup() {
 main() {
     check_root
     check_params "$@"
-
+    export THMDCIP="$1"
     local THMDCIP="$1"
     local OPENVPN_CONFIG="$2"
-
+    #killing existiong openvpn connections:
+    echo "Killing old openvpn session..."
+    sudo pkill openvpn
+    sleep 5
     # Start OpenVPN with the provided configuration file
+    echo "Starting new openvpn session..."
     sudo openvpn --config "$OPENVPN_CONFIG" &
 
     # Wait for the connection to establish (adjust sleep time as needed)
@@ -43,31 +46,31 @@ main() {
 
     # Check if THMDCIP is reachable
     if ping -c 1 "$THMDCIP" &>/dev/null; then
-        echo "$THMDCIP is reachable"
+        echo "[+] $THMDCIP is reachable"
 
         # Backup the original /etc/resolv.conf
         cp /etc/resolv.conf /etc/resolv.conf.backup
 
         # Update /etc/resolv.conf
-        echo "nameserver $THMDCIP" > /etc/resolv.conf
-        echo "options timeout:1 attempts:2" >> /etc/resolv.conf
+        echo -e "nameserver $THMDCIP \nnameserver 1.1.1.1 \noptions timeout:1 \noptions attempts:2" > /etc/resolv.conf
+   #     echo "options timeout:1 attempts:2" >> /etc/resolv.conf
 
         # Restart networking service
         sudo systemctl restart networking.service
 
         # Perform nslookup checks and report success
         perform_nslookup thmdc.za.tryhackme.com
-        perform_nslookup google.com
-        perform_nslookup tryhackme.com "$THMDCIP"
+        perform_nslookup "tryhackme.com $THMDCIP"
+        perform_nslookup tryhackme.com
 
         # Check if ntlmauth.za.tryhackme.com responds and report success
         if nslookup ntlmauth.za.tryhackme.com &>/dev/null; then
-            echo "ntlmauth.za.tryhackme.com is reachable"
+            echo "[+] ntlmauth.za.tryhackme.com is reachable"
         else
-            echo "ntlmauth.za.tryhackme.com is not reachable"
+            echo "[-] ntlmauth.za.tryhackme.com is not reachable"
         fi
     else
-        echo "$THMDCIP is not reachable"
+        echo "[-] $THMDCIP is not reachable"
     fi
 }
 
